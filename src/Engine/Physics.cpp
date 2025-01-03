81eb09289a7eebfacceac8c3bfad0be892a22b1e
Bekir Gülestan <gulestanbekir@gmail.com>
2025-01-03 05:23:20 -0600

    use of timeDelta

--- src/Engine/Physics.cpp
+++ src/Engine/Physics.cpp
@@ -31,7 +31,6 @@ void Physics::Init() {
 void Physics::Update() {
     // Warning: this is not the correct way to get the time step
     float timeStep = LowRenderer::getDeltaTime();
-    float timeStep = 1.0f / 60.0f;
     int subStepCount = 4;
 
 
